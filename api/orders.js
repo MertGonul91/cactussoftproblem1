000c774f28fd898c99e379730676f1fff589e6ca
Mert Gönül <mertgonul91@gmail.com>
2021-10-31 17:29:37 +0100

    Set: Api

---
+++ api/orders.js
@@ -0,0 +1,129 @@
+module.exports = [
+    {
+        "id": "1",
+        "customer-id": "1",
+        "items": [
+            {
+                "product-id": "B102",
+                "quantity": "10",
+                "unit-price": "4.99",
+                "total": "49.90"
+            }
+        ],
+        "total": "49.90"
+    },
+    {
+        "id": "2",
+        "customer-id": "2",
+        "items": [
+            {
+                "product-id": "B102",
+                "quantity": "5",
+                "unit-price": "4.99",
+                "total": "24.95"
+            }
+        ],
+        "total": "24.95"
+    },
+    {
+        "id": "3",
+        "customer-id": "3",
+        "items": [
+            {
+                "product-id": "A101",
+                "quantity": "2",
+                "unit-price": "9.75",
+                "total": "19.50"
+            },
+            {
+                "product-id": "A102",
+                "quantity": "1",
+                "unit-price": "49.50",
+                "total": "49.50"
+            }
+        ],
+        "total": "69.00"
+    },
+    {
+        "id": "4",
+        "customer-id": "4",
+        "items": [
+            {
+                "product-id": "B102",
+                "quantity": "7",
+                "unit-price": "9.75",
+                "total": "68.25"
+            },
+            {
+                "product-id": "A102",
+                "quantity": "2",
+                "unit-price": "49.50",
+                "total": "49.50"
+            }
+        ],
+        "total": "117.75"
+    },
+    {
+        "id": "5",
+        "customer-id": "4",
+        "items": [
+            {
+                "product-id": "B102",
+                "quantity": "7",
+                "unit-price": "9.75",
+                "total": "68.25"
+            },
+            {
+                "product-id": "A102",
+                "quantity": "1",
+                "unit-price": "49.50",
+                "total": "49.50"
+            }
+        ],
+        "total": "117.75"
+    },
+    {
+        "id": "6",
+        "customer-id": "4",
+        "items": [
+            {
+                "product-id": "A101",
+                "quantity": "2",
+                "unit-price": "9.75",
+                "total": "19.50"
+            },
+        ],
+        "total": "19.50"
+    },
+    {
+        "id": "7",
+        "customer-id": "4",
+        "items": [
+            {
+                "product-id": "A101",
+                "quantity": "1",
+                "unit-price": "9.75",
+                "total": "9.75"
+            },
+            {
+                "product-id": "A102",
+                "quantity": "1",
+                "unit-price": "49.50",
+                "total": "49.50"
+            },
+            {
+                "product-id": "B102",
+                "quantity": "7",
+                "unit-price": "9.75",
+                "total": "68.25"
+            },
+            {
+                "product-id": "B101",
+                "quantity": "10",
+                "unit-price": "9.75",
+                "total": "97.50"
+            },
+        ],
+        "total": "69.00"
+    },
+]
